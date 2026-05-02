953f4b147e24b14c277d1d9a70f11b3bdbac95db
sseooh <sseooh@gmail.com>
2026-05-02 18:15:10 +0900

    hotfix: releaserc 파일 수정(브랜치 관련)

--- .releaserc.js
+++ .releaserc.js
@@ -6,38 +6,40 @@ module.exports = {
       {
         preset: "conventionalcommits",
         parserOpts: {
+          headerPattern: /^(\w*)(?:\((.*)\))?!?: (.*)$/,
+          headerCorrespondence: ["type", "scope", "subject"],
           noteKeywords: ["BREAKING CHANGE", "BREAKING"],
-          issuePrefixes: ["#"],
+          issuePrefixes: ["#"]
         },
         releaseRules: [
+          // Breaking changes -> major
           { breaking: true, release: "major" },
-          { revert: true, release: "patch" },
-          { type: "feat", breaking: true, release: "major" },
-          { type: "feat", release: "minor" },
+          // All feat commits -> major (including feat!)
+          { type: "feat", release: "major" },
+          { type: "revert", release: "patch" },
           { type: "fix", release: "patch" },
-          { type: "perf", release: "patch" },
-        ],
-      },
+          { type: "perf", release: "patch" }
+        ]
+      }
     ],
     [
       "@semantic-release/release-notes-generator",
       {
-        preset: "conventionalcommits",
-      },
+        preset: "conventionalcommits"
+      }
     ],
     [
       "@semantic-release/gitlab",
       {
-        gitlabUrl: "https://lab.ssafy.com",
-      },
+        gitlabUrl: "https://lab.ssafy.com"
+      }
     ],
     [
       "@semantic-release/git",
       {
         assets: ["k8s/*.yaml", "package.json"],
-        message:
-          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-      },
-    ],
-  ],
+        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+      }
+    ]
+  ]
 };
